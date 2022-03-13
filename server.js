635eee771027e34f9b16382659201a12c33ce86c
LloydDub <lloydrww@gmail.com>
2022-03-12 19:43:22 -0700

    scoping error fixed

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const { hash } = require("bcrypt");
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(salt);
+    console.log(hashedPassword);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.listen(3000);
+
+//use bcrypt to make salt
