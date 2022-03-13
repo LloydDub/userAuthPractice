4d45e68eff3b6d14b2aa192e82836b8f90da85dc
LloydDub <lloydrww@gmail.com>
2022-03-12 20:18:40 -0700

    login route, user auth and login prepped

--- server.js
+++ server.js
@@ -15,11 +15,26 @@ app.post("/users", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    console.log(salt);
-    console.log(hashedPassword);
+
     const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("success");
+    } else {
+      res.send("bad login");
+    }
   } catch {
     res.status(500).send();
   }
